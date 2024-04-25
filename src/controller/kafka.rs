2dc001688931def6be0e69b60b5200d4ae6aabce
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2024-04-25 04:44:26 -0400

    feat: Kafka consumer config.

--- src/controller/kafka.rs
+++ src/controller/kafka.rs
@@ -26,7 +26,7 @@ impl ConsumerContext for CustomContext {
 type LoggingConsumer = StreamConsumer<CustomContext>;
 
 pub fn create_consumer(brokers: &str, group_id: &str, topics: &[&str]) -> StreamConsumer<CustomContext> {
-    let context = CustomContext;
+	let context: CustomContext = CustomContext;
 
     let consumer: LoggingConsumer =  ClientConfig::new()
             .set("group.id", group_id)
@@ -34,12 +34,23 @@ pub fn create_consumer(brokers: &str, group_id: &str, topics: &[&str]) -> Stream
             .set("enable.partition.eof", "false")
             .set("session.timeout.ms", "6000")
             .set("enable.auto.commit", "true")
+		.set("message.max.bytes", "1000000000")
+		.set("message.copy.max.bytes", "1000000000")
+		.set("receive.message.max.bytes", "2147483647")
+		.set("socket.send.buffer.bytes", "100000000")
+		.set("socket.receive.buffer.bytes", "100000000")
+		.set("queued.max.messages.kbytes", "2097151")
+		.set("fetch.message.max.bytes", "1000000000")
+		.set("max.partition.fetch.bytes", "1000000000")
+		.set("fetch.max.bytes", "2147483135")
         .set_log_level(RDKafkaLogLevel::Debug)
         .create_with_context(context)
-        .expect("Consumer creation failed");
+		.expect("Consumer creation failed")
+	;
 
     consumer
         .subscribe(&topics.to_vec())
-        .expect("Can't subscribe to specified topics");
+		.expect("Can't subscribe to specified topics")
+	;
     consumer
 }
